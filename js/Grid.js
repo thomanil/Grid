cecda84901c1e9aab97a0ef938d7437374d22e66
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2011-03-06 12:22:23 +0100

    Reworked api to have single constructor for wrapping regular array as well as initalizing empty grids

--- js/Grid.js
+++ js/Grid.js
@@ -17,113 +17,127 @@ if (typeof Object.create !== 'function') {
     };
 }
 
-var Grid = Object.create([]);
 
-Grid.newInstance = function(width, height) {
-	var grid = Object.create(Grid);
-	var emptyRow = [];
+var Grid = (function() {
+
+    var gridPrototype = Object.create([]);
+
+    gridPrototype.wrap = function(twoDimArray) {
+		// TODO handle different sized rows. Truncate or throw exception?
 	
-	_.times(height, function(i) {
-		var row = [];
-		_.times(width, function(j) {
-			row.push(undefined);
-		});
-		grid.push(row);
-	});
-	
-	return grid;
-};
+       	 var newGrid = gridPrototype.getEmptyGrid(0, 0);
+            _(twoDimArray).each(function(row, i) {
+                newGrid.push(twoDimArray[i]);
+            });
+        return newGrid;
+    };
 
-Grid.wrap = function(twoDimArray) {
-	if (twoDimArray.__proto__) {
-		twoDimArray.__proto__ = Grid;
-	} else {
-		var newArr = Grid.newInstance(0, 0);
+    gridPrototype.equals = function(otherGrid) {
+        if (this.height() !== otherGrid.height()) {
+            return false;
+        }
 
-		_(twoDimArray).each(function(row, i) {
-			newArr.push(twoDimArray[i]);
-		});
+        if (this.width() !== otherGrid.width()) {
+            return false;
+        }
 
-		twoDimArray = newArr;
-	}
+        var areEqual = true;
+        this.each(function(cell, x, y) {
+            if (cell !== otherGrid.get(x, y)) {
+                areEqual = false;
+            }
+        });
+        return areEqual;
+    };
 
-	return twoDimArray;
-};
+    gridPrototype.each = function(operation) {
+        _(this).each(function(row, y) {
+            _(row).each(function(cell, x) {
+                operation(cell, x, y);
+            });
+        });
+    };
 
-Grid.equals = function(otherGrid) {	
-	if (this.height() !== otherGrid.height()) {
-		return false;
-	}
+    gridPrototype.map = function(operation) {
+        var mappedArray = this.getEmptyGrid(this.width(), this.height());
 
-	if (this.width() !== otherGrid.width()) {
-		return false;
-	}
-	
-	var areEqual = true;
-	this.each(function(cell, x, y) {
-		if (cell !== otherGrid.get(x, y)) {
-		  areEqual = false;
+        this.each(function(cell, x, y) {
+            var mappedValue = operation(cell, x, y);
+            mappedArray.set(x, y, mappedValue);
+        });
+
+        return mappedArray;
+    };
+
+    gridPrototype.get = function(x, y) {
+        if (x < 0 || x > (this.width() - 1)) {
+            return undefined;
+        }
+
+        if (y < 0 || y > (this.height() - 1)) {
+            return undefined;
+        }
+
+        return this[y][x];
+    };
+
+    gridPrototype.set = function(x, y, value) {
+        if (x < 0 || x > (this.width() - 1)) {
+            throw "Trying to index x pos outside of Grid bounds";
+        }
+
+        if (y < 0 || y > (this.height() - 1)) {
+            throw "Trying to index y pos outside of Grid bounds";
+        }
+
+        this[y][x] = value;
+    };
+
+    gridPrototype.width = function() {
+        return this[0].length;
+    };
+
+    gridPrototype.height = function() {
+        return this.length;
+    };
+
+    gridPrototype.eachRow = function(operation) {
+        _(this).each(function(row) {
+            operation(row);
+        });
+    };
+
+ 	gridPrototype.getEmptyGrid = function(width, height) {
+        var grid = Object.create(gridPrototype);
+        var emptyRow = [];
+
+        _.times(height,
+        function(i) {
+            var row = [];
+            _.times(width,
+            function(j) {
+                row.push(undefined);
+            });
+            grid.push(row);
+        });
+
+        return grid;
+    };
+
+    return function() { // Constructor	
+		if(_.isArray(arguments[0])){
+			return gridPrototype.wrap(arguments[0]);
+		} else if(typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ){
+			return gridPrototype.getEmptyGrid(arguments[0], arguments[1]);
+		} else {
+			throw "Not supported usage of constructor.Usage: new Grid(xDim, yDim) OR new Grid(arrayOfArrays)";
 		}
-	});
-	return areEqual;
-};
+    };
 
-Grid.each = function(operation) {
-	_(this).each(function(row, y) {
-		_(row).each(function(cell, x){
-			operation(cell, x, y);
-		});
-	});	
-};
+})();
 
-Grid.map = function(operation) {
-	var mappedArray = this.newInstance(this.width(), this.height());	
-		
-	this.each(function(cell, x, y) {
-		var mappedValue = operation(cell, x, y);
-		mappedArray.set(x, y, mappedValue);
-	});
-	
-	return mappedArray;
-};
 
-Grid.get = function(x, y) {
-	if (x < 0 || x > (this.width() - 1)) {
-		return undefined;
-	}
-	
-	if (y < 0 || y > (this.height() - 1)) {
-		return undefined;
-	}
-	
-	return this[y][x];
-};
 
-Grid.set = function(x, y, value) {
-	if (x < 0 || x > (this.width() - 1)) {
-	 	throw "Trying to index x pos outside of Grid bounds";
-	}
-	
-	if (y < 0 || y > (this.height() - 1)) {
-		throw "Trying to index y pos outside of Grid bounds";
-	}
-	
-	this[y][x] = value;
-};
-
-Grid.width = function() {
-	return this[0].length;
-};
-
-Grid.height = function() {
-	return this.length;
-};
-
-Grid.eachRow = function(operation) {
-	_(this).each(function(row) {
-		operation(row);
-	});
-};
 
 
 
--- test/GridTests.js
+++ test/GridTests.js
@@ -1,152 +1,147 @@
 TestCase("Grid Tests", {
-	
-    testProjectNamespace:function(){
-	   assertTrue("Checking that project namespace exists", Grid !== undefined);
+
+	testNonHandledConstructorException: function(){
+		var threwConstructorException = true;
+		try {
+            var grid = new Grid("Expects array of arrays or length/width numbers");
+            threwConstructorException = false;
+        } catch(ex1) {
+            assertEquals("Not supported usage of constructor.Usage: new Grid(xDim, yDim) OR new Grid(arrayOfArrays)", ex1);
+            }
+        if (!threwConstructorException) {
+            fail("Should have triggered exception giving suggested usage of Grid constructor.");
+        }
+	},
+
+    testCreate: function() {
+        var expected = new Grid([[undefined, undefined],
+        						[undefined, undefined]]);
+        var actual = new Grid(2, 2);
+        assertTrue(actual.equals(expected));
     },
 
-	testCreate : function() {
-		var expected = Grid.wrap([[undefined,undefined],
-					     		 [undefined,undefined]]);
-		var actual = Grid.newInstance(2,2);
-		assertTrue(actual.equals(expected));
-	},
+    testCompareEqualGrids: function() {
+        var equal1 = new Grid([[undefined, undefined],
+        						[undefined, undefined]]);
+        var equal2 = new Grid([[undefined, undefined],
+        						[undefined, undefined]]);
+        assertTrue(equal1.equals(equal2));
+    },
 
-	testCompareEqualGrids : function() {
-		var equal1 = Grid.wrap([[undefined,undefined],
-					  			[undefined,undefined]]);
-		var equal2 = Grid.wrap([[undefined,undefined],
-					  [undefined,undefined]]);
-		assertTrue(equal1.equals(equal2));
-	},
-	
-	testCompareDifferentSizedGrids : function() {
-		var small = Grid.wrap([[undefined,undefined],
-					  			[undefined,undefined]]);	
-		var large = Grid.wrap([[undefined,undefined,undefined],
-					  			[undefined,undefined,undefined],
-					  			[undefined,undefined,undefined]]);
-		assertFalse(small.equals(large));
-	},
-	
-	testCompareGridsWithDifferentContents : function() {
-		var none = Grid.wrap([[undefined,undefined],
-				  	 			[undefined,undefined]]);	
-		var some = Grid.wrap([[2,undefined],
-								[undefined,2]]);
-		assertFalse(none.equals(some));
-	},
+    testCompareDifferentSizedGrids: function() {
+        var small = new Grid([[undefined, undefined],
+        					[undefined, undefined]]);
+        var large = new Grid([[undefined, undefined, undefined],
+        					[undefined, undefined, undefined],
+        					[undefined, undefined, undefined]]);
+        assertFalse(small.equals(large));
+    },
 
-	testEach : function() {
-		var grid = Grid.wrap([[1,2],
-					 		 [3,4]]);
-		
-		var expectedSum = 10;
-		var actualSum = 0;
-		
-		grid.each(function(cell) {
-			actualSum += cell;
-		});
-		
-		assertEquals(expectedSum, actualSum);		
-	},
-	
-	testMap : function() {	
-		var grid = Grid.wrap([[1,5],
-					 		  [10,100]]);
-		var expected = Grid.wrap([[2,6],
-					 			 [11,101]]);	
-					
-		var actual = grid.map(function(cell) {
-			return cell + 1;
-		});
-		
-		assertTrue(actual.equals(expected));		
-	},
-	
-	testGet : function() {
-		var grid = Grid.wrap([[1,5],
-					 		[10,100]]);
-					
-		assertEquals(grid.get(0, 0), 1);
-		assertEquals(grid.get(1, 0), 5);		
-		assertEquals(grid.get(0, 1), 10);		
-		assertEquals(grid.get(1, 1), 100);
-		
-		assertEquals(grid.get(-1, 0), undefined);
-		assertEquals(grid.get(0, -1), undefined);
-		assertEquals(grid.get(2, 0), undefined);
-		assertEquals(grid.get(0, 2), undefined);
-	},
-	
-	testSet : function() {
-		var grid = Grid.newInstance(2,2);
-		
-		grid.set(0, 0, 123);
-		assertEquals(grid.get(0, 0), 123);
-		
-		grid.set(1, 1, 234);
-		assertEquals(grid.get(1, 1), 234);
-		
-		var threwException1 = true;
-		try	{
-			grid.set(-1, 1, "outside");
-			threwException1 = false;
-		} catch (ex1) {
-			// Expecting to fail when we go outside Grid bounds
-		}
-		if (!threwException1) {
-			fail("Should have triggered exception by indexing outside of Grid"); 
-		}
-		
-		var threwException2 = true;
-try	{
-			grid.set(2, 1, "outside");
-			threwException2 = false;
-		} catch (ex2) {
-			// Expecting to fail when we go outside Grid bounds
-		}
-		if (!threwException2) {
-			fail("Should have triggered exception by indexing outside of Grid"); 
-		}
-	
-	},
-	
-	testWidth : function() {
-		var grid = Grid.newInstance(3,2);
-		assertEquals(3, grid.width());
-	},
-	
-	testHeight : function() {
-		var grid = Grid.newInstance(3,2);
-		assertEquals(2, grid.height());
-	},
-	
-	testEachRow : function() {
-		var grid = Grid.wrap([[1,5],
-					 		[10,100]]);
-		var sumOfCells = 0;
-		
-		grid.eachRow(function(row) {
-			_(row).each(function(cell) {
-				sumOfCells += cell;
-			});
-		});
-		
-		assertEquals(116, sumOfCells);
-					
-	},
+    testCompareGridsWithDifferentContents: function() {
+        var none = new Grid([[undefined, undefined],
+        					[undefined, undefined]]);
+        var some = new Grid([[2, undefined],
+        					[undefined, 2]]);
+        assertFalse(none.equals(some));
+    },
 
-	testWrapNo__proto__: function () {
-		var twoDimArray = [[undefined,undefined], [undefined,undefined]];
-		twoDimArray.__proto__ = null;
-		var expected = Grid.wrap(twoDimArray);
-		var actual = Grid.newInstance(2,2);
+    testEach: function() {
+        var grid = new Grid([[1, 2],
+        					[3, 4]]);
 
-		assertTrue(actual.equals(expected));
-	},
-	
-	
-	
-	
+        var expectedSum = 10;
+        var actualSum = 0;
+
+        grid.each(function(cell) {
+            actualSum += cell;
+        });
+
+        assertEquals(expectedSum, actualSum);
+    },
+
+    testMap: function() {
+        var grid = new Grid([[1, 5],
+        					[10, 100]]);
+        var expected = new Grid([[2, 6],
+        						[11, 101]]);
+
+        var actual = grid.map(function(cell) {
+            return cell + 1;
+        });
+
+        assertTrue(actual.equals(expected));
+    },
+
+    testGet: function() {
+        var grid = new Grid([[1, 5],
+        					[10, 100]]);
+
+        assertEquals(grid.get(0, 0), 1);
+        assertEquals(grid.get(1, 0), 5);
+        assertEquals(grid.get(0, 1), 10);
+        assertEquals(grid.get(1, 1), 100);
+
+        assertEquals(grid.get( - 1, 0), undefined);
+        assertEquals(grid.get(0, -1), undefined);
+        assertEquals(grid.get(2, 0), undefined);
+        assertEquals(grid.get(0, 2), undefined);
+    },
+
+    testSet: function() {
+        var grid = new Grid(2, 2);
+
+        grid.set(0, 0, 123);
+        assertEquals(grid.get(0, 0), 123);
+
+        grid.set(1, 1, 234);
+        assertEquals(grid.get(1, 1), 234);
+
+        var threwException1 = true;
+        try {
+            grid.set( - 1, 1, "outside");
+            threwException1 = false;
+        } catch(ex1) {
+            // Expecting to fail when we go outside Grid bounds
+            }
+        if (!threwException1) {
+            fail("Should have triggered exception by indexing outside of Grid");
+        }
+
+        var threwException2 = true;
+        try {
+            grid.set(2, 1, "outside");
+            threwException2 = false;
+        } catch(ex2) {
+            // Expecting to fail when we go outside Grid bounds
+            }
+        if (!threwException2) {
+            fail("Should have triggered exception by indexing outside of Grid");
+        }
+
+    },
+
+    testWidth: function() {
+        var grid = new Grid(3, 2);
+        assertEquals(3, grid.width());
+    },
+
+    testHeight: function() {
+        var grid = new Grid(3, 2);
+        assertEquals(2, grid.height());
+    },
+
+    testEachRow: function() {
+        var grid = new Grid([[1, 5],
+        					[10, 100]]);
+        var sumOfCells = 0;
+
+        grid.eachRow(function(row) {
+            _(row).each(function(cell) {
+                sumOfCells += cell;
+            });
+        });
+
+        assertEquals(116, sumOfCells);
+    }
 
 });
